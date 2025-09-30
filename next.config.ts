d6379b22135d9416cd7b528c43fe1965a2401d6e
Arnold Rodriguez <arnolddavidrd@gmail.com>
2025-09-30 16:01:25 -0500

    feat: update webpack configuration to handle LICENSE files from hrana-client package

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /node_modules[\\/]@libsql[\\/]hrana-client[\\/]LICENSE$/,
+      type: "asset/source"
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
