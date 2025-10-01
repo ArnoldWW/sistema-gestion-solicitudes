6e59022e6c3565de16f46156ec842e729ce976dc
Arnold Rodriguez <arnolddavidrd@gmail.com>
2025-09-30 23:58:56 -0500

    style: enhance user information display in DashboardLayout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -34,7 +34,12 @@ export default async function DashboardLayout({
     <div className="flex min-h-screen overflow-hidden">
       <Sidebar>
         <div>
-          <h1>Gestion de solicitudes</h1>
+          <h1 className="flex flex-col items-start">
+            <span className="text-sm block text-blue-500 bg-blue-200 p-1">
+              {user.role}
+            </span>
+            Gestion de solicitudes
+          </h1>
           <nav className="flex flex-col mt-4 gap-4">
             {linksByRole[user.role].map((link) => (
               <Link key={link.href} href={link.href}>
@@ -45,10 +50,29 @@ export default async function DashboardLayout({
         </div>
 
         <form action={logoutUser} className="flex flex-col gap-4">
-          <p>
-            Usuario: {user?.name} -{" "}
-            <small className="text-neutral-500">{user?.role}</small>
-          </p>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-user"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+              <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            </svg>
+
+            <p>
+              {user?.name} -{" "}
+              <span className="text-blue-500">({user?.email})</span>
+            </p>
+          </div>
 
           <button type="submit" className="btn w-full">
             Cerrar sesión
