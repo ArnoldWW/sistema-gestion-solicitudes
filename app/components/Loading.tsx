b940cfd0981c0cd88de78db3220f0a73944bfc6c
arnold rodriguez <arnolddavidrd@gmail.com>
2026-05-20 19:02:22 -0500

    refactor: remove unnecessary React imports

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Loading() {
   return (
     <div className="w-full flex items-center justify-center">
--- app/dashboard/customer/[id]/loading.tsx
+++ app/dashboard/customer/[id]/loading.tsx
@@ -1,5 +1,4 @@
 import Loading from "@/app/components/Loading";
-import React from "react";
 
 export default function loading() {
   return <Loading />;
